e7c255810673522c51c6a3e645e280bdb46c2b9e
Chris Coffey <cmcof273@gmail.com>
2020-10-27 18:05:27 -0400

    Wire up the app

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
 import Protolude
+import qualified Data.Text as T
+import Statsbot
 
 main :: IO ()
-main = pure ()
+main = do
+    args <- getArgs
+    reportFile <- maybe (print "You must provide a report .yaml file" *> exitFailure)
+                        pure $
+                        head args
+    runStatsbot reportFile
